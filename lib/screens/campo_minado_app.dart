1dcbd47f140275ff9b6146c4302429a62523b52c
Fernando Candiani <candianirfernando@gmail.com>
2022-12-19 11:32:24 -0300

    Registrar imagens e componente campo

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
+import '../components/campo_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
 
-  _reiniciar() {
+  void _reiniciar() {
     print('Reiniciar...');
+  }
+
+  void _abrir(Campo campo) {
+    print('Abrir');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('Alternar Marcação');
   }
 
   @override
   Widget build(BuildContext context) {
+    Campo campo = Campo(linha: 0, coluna: 0);
+    try {
+      campo.minar();
+      campo.abrir();
+    } on ExplosaoException {}
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -17,7 +34,11 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: const Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
